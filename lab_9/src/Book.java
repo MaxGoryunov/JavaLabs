b3bdee223c8cd304dfe2556dd59004b676c5d846
Max Goryunov <maximthehedgehog@yandex.ru>
2023-12-06 00:43:12 +0300

    add Book class

---
+++ lab_9/src/Book.java
@@ -0,0 +1,25 @@
+public class Book {
+    private String title;
+    private Integer year;
+
+    public Book(String title, Integer year) {
+        this.title = title;
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+}
