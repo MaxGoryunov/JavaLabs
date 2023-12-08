d4543034c863d9ec039d2672e1b179093f09446e
Max Goryunov <maximthehedgehog@yandex.ru>
2023-12-08 23:53:25 +0300

    add BitsOf class

--- Huffman/src/io/BitsOf.java
+++ Huffman/src/io/BitsOf.java
@@ -1,5 +1,23 @@
 package io;
 
+import java.io.InputStream;
+
 public class BitsOf {
-    private
+    private OneBit bit;
+
+    public BitsOf(InputStream input) {
+        this.bit = new OneBit(input);
+    }
+
+    public int bits(int count) throws Exception {
+        int res = 0;
+        for (int i = 0; i < count; ++i) {
+            int bit = this.bit.value();
+            if (bit == -1) {
+                return bit;
+            }
+            res = (res << 1) | bit;
+        }
+        return res;
+    }
 }
--- Huffman/src/tree/Tree.java
+++ Huffman/src/tree/Tree.java
@@ -1,5 +1,5 @@
 package tree;
 
 public final class Tree {
-    private
+
 }
